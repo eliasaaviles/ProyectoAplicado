58f4f712225bed394882de8b6962693aec591ff2
Allg17 <albert_lopez@ucne.edu.do>
2018-06-25 11:30:08 -0400

    Creando BLL usuario

--- FacturacionAplicado/BLL/UsuarioBLL.cs
+++ FacturacionAplicado/BLL/UsuarioBLL.cs
@@ -3,10 +3,190 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using FacturacionAplicado.Entidades;
 
 namespace FacturacionAplicado.BLL
 {
     public class UsuarioBLL
     {
+
+        public static DataTable Consultar()
+        {
+
+
+            DataTable dt = new DataTable();
+
+            MySqlDataAdapter con = new MySqlDataAdapter("select * from usuarios", UI.Menu.MenuMasVentas.RetornarConexion());
+            try
+            {
+
+
+                con.Fill(dt);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return dt;
+        }
+
+        public static bool Eliminar(string id)
+        {
+
+            MySqlDataAdapter dater = new MySqlDataAdapter();
+            bool estado = true;
+            try
+            {
+
+                dater.DeleteCommand = new MySqlCommand(" delete from usuarios where IdUsuario=" + id, UI.Menu.MenuMasVentas.RetornarConexion());
+
+                dater.DeleteCommand.Connection = UI.Menu.MenuMasVentas.RetornarConexion();
+                dater.DeleteCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                estado = false;
+                throw;
+
+            }
+
+            return estado;
+        }
+
+
+        public static bool Guardar(Usuario user)
+        {
+            MySqlDataAdapter dater = new MySqlDataAdapter();
+            bool estado = true;
+            try
+            {
+
+                dater.InsertCommand = new MySqlCommand("insert into Usuarios (Nombre,Clave,NombreUsuario,Fecha,Comentario) values ('" + user.Nombre + "','" + user.Clave + "','" + user.NombreUsuario + "','" + user.Fecha + "','" + user.Comentario + "')", UI.Menu.MenuMasVentas.RetornarConexion());
+
+                dater.InsertCommand.Connection = UI.Menu.MenuMasVentas.RetornarConexion();
+                dater.InsertCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                estado = false;
+                throw;
+
+            }
+
+            return estado;
+        }
+
+
+        //asi se pone un datatable en una lista 
+        public static List<Usuario> Buscar()
+        {
+
+            DataTable dt = new DataTable();
+
+
+            MySqlDataAdapter con = new MySqlDataAdapter("select * from Usuarios", UI.Menu.MenuMasVentas.RetornarConexion());
+            try
+            {
+
+
+                con.Fill(dt);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+            List<Usuario> listName = dt.AsEnumerable().Select(m => new Usuario()
+            {
+                IdUsuario = m.Field<int>("IdUsuario"),
+                Nombre = m.Field<string>("Nombre"),
+                Clave = m.Field<string>("Clave"),
+                NombreUsuario = m.Field<string>("NombreUsuario"),
+                Fecha = m.Field<DateTime>("Fecha"),
+                Comentario = m.Field<string>("Comentario")
+            }).ToList();
+
+
+            return listName;
+        }
+
+        public static List<Usuario> Buscar(string id)
+        {
+
+            DataTable dt = new DataTable();
+
+
+            MySqlDataAdapter con = new MySqlDataAdapter("select * from Usuarios where IdUsuario=" + id, UI.Menu.MenuMasVentas.RetornarConexion());
+            try
+            {
+
+
+                con.Fill(dt);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+            List<Usuario> listName = dt.AsEnumerable().Select(m => new Usuario()
+            {
+                IdUsuario = m.Field<int>("IdUsuario"),
+                Nombre = m.Field<string>("Nombre"),
+                Clave = m.Field<string>("Clave"),
+                NombreUsuario = m.Field<string>("NombreUsuario"),
+                Fecha = m.Field<DateTime>("Fecha"),
+                Comentario = m.Field<string>("Comentario")
+            }).ToList();
+
+
+            return listName;
+        }
+
+
+
+        public static bool Modificar(Usuario user)
+        {
+            MySqlDataAdapter dater = new MySqlDataAdapter();
+            bool estado = true;
+            try
+            {
+
+
+                dater.UpdateCommand = new MySqlCommand("update Usuarios set IdUsuario= '" + user.IdUsuario + "', Nombre = '" + user.Nombre + "', Clave = '" + user.Clave + "', NombreUsuario = '" + user.NombreUsuario + "', Fecha= '" + user.Fecha + "',Comentario='" + user.Comentario + "'where Idproducto = '" + user.IdUsuario + "'", UI.Menu.MenuMasVentas.RetornarConexion());
+
+                dater.UpdateCommand.Connection = UI.Menu.MenuMasVentas.RetornarConexion();
+                dater.UpdateCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                estado = false;
+                throw;
+
+            }
+
+            return estado;
+
+        }
     }
 }
